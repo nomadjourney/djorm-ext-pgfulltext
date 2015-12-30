6f408d522e7fd6299c1f2e1c13da58c39cf97cc1
Nizam Sayeed <nizam@nomadjourney.com>
2015-12-30 19:31:04 +0000

    Another module reference fix.

--- djorm_pgfulltext/utils.py
+++ djorm_pgfulltext/utils.py
@@ -1,10 +1,9 @@
 import psycopg2.extensions
 
-from django.db import connection
 from django.utils.text import force_text
 
 
 def adapt(text):
     a = psycopg2.extensions.adapt(force_text(text))
-    a.prepare(connection.connection)
+    a.prepare(psycopg2.extensions.connection)
     return a
